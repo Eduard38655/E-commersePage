7eba3abd414ee7be4477a6aaa2ebf6a7cfcc8b8b
Eduardo <eduao154@gmail.com>
2025-07-10 22:02:11 -0400

    Update NodeRouter.js

--- GourmetGo/NodeRouter.js
+++ GourmetGo/NodeRouter.js
@@ -9,8 +9,14 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const Allow=['http://localhost:3000/','https://eduard38655.github.io/E-commersePage/']
+ const corsOptions = {
+    origin:Allow,
+    credentials: true,
+    optionSuccessStatus: 200
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
