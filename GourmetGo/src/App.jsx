37b4781f092fcf0e64852a7f885f21b802161b48
Eduardo <eduao154@gmail.com>
2025-07-11 20:38:57 -0400

    Final Commit

--- GourmetGo/src/App.jsx
+++ GourmetGo/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DataBaseProvider from "../src/Components/Context/DataBase.jsx";
 import MainPage from "./Components/Pages/MainPage.jsx";
+//App copm.
 function App() {
  
   return (
